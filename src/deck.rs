7411a7c404256077f93f3fa2b55641caed827195
riyaverma23 <riyarverma23@gmail.com>
2021-12-02 05:13:10 +0000

    added print deck function -e

--- src/deck.rs
+++ src/deck.rs
@@ -1,22 +1,37 @@
 pub mod card;
+use crate::card::IntToSuit;
+use crate::card::Card;
+
 pub struct Deck {
     card_deck: Vec<Card>,
 }
 
 impl Deck {
     pub fn new() -> Deck {
-        let deck: Vec<Card> = Vec::new();
+        let mut deck: Vec<Card> = Vec::new();
 
         for i in 1..13 {
             for j in 1..4 {
                 let c = Card {
                     val : i,
-                    suit : card::IntToSuit(j),
+                    suit : IntToSuit(j).unwrap(),
                 };
-                deck.pushback(c);
+                deck.push(c);
             }
         }
-        return deck;
+        let d : Deck = Deck {
+            card_deck : deck, 
+        };
+        return d;
+    }
+
+    pub fn PrintDeck(&self) {
+
+        for i in 0..self.card_deck.len() {
+            self.card_deck[i].PrintCard();
+
+        }
+
     }
 
 
--- target/debug/deps/poker-767d81cf5585137d.d
+++ target/debug/deps/poker-767d81cf5585137d.d
@@ -1,6 +1,7 @@
-/home/vagrant/src/Rust Classwork/Multiplayer-Poker-Rust/target/debug/deps/poker-767d81cf5585137d: src/main.rs src/card.rs
+/home/vagrant/src/Rust Classwork/Multiplayer-Poker-Rust/target/debug/deps/poker-767d81cf5585137d: src/main.rs src/card.rs src/deck.rs
 
-/home/vagrant/src/Rust Classwork/Multiplayer-Poker-Rust/target/debug/deps/poker-767d81cf5585137d.d: src/main.rs src/card.rs
+/home/vagrant/src/Rust Classwork/Multiplayer-Poker-Rust/target/debug/deps/poker-767d81cf5585137d.d: src/main.rs src/card.rs src/deck.rs
 
 src/main.rs:
 src/card.rs:
+src/deck.rs:
